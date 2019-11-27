Fix warping&normalization.

rogp/core.py
@@ -123,27 +123,25 @@ class Warped(Standard):
         y_norm = self.norm.y.normalize(y)
         return self._warp_deriv(y_norm)
 
-    def predict_mu(self, x, z, cons):
+    def predict_mu(self, x, y, cons):
         """ Predict mean from GP at x. """
         # Scale input
         x_norm = self.norm.x.normalize(x)
         # Calculate mean
-        y = self._predict_mu(x_norm)
-        # Scale in observation space
-        z_norm = self.norm.y.normalize(z)
+        z = self._predict_mu(x_norm)
         # Set to prediction y in latent space
-        diff = self.warp(z_norm) - y
+        diff = self.warp(y) - z
         for d in np.nditer(diff, ['refs_ok']):
             cons.add(d.item() == 0)
-        return z
+        return y
 
     def predict_mu_latent(self, x):
         """ Predict mean from GP at x in latent space (normalized). """
         # Scale input
         x_norm = self.norm.x.normalize(x)
         # Calculate mean
-        y = self._predict_mu(x_norm)
-        return y
+        z = self._predict_mu(x_norm)
+        return z
 
     def predict_cov_latent(self, x):
         """ Predict covariance between two points from GP. """

tests/test_core.py
@@ -50,14 +50,15 @@ def wgp(scope='module'):
 class TestWarping:
 
     def test_warping_numeric(self, wgp):
-        Y = np.random.uniform(size=(5, 1))
-        Z_rogp = wgp.warp(Y)
-        Z_gpy = wgp.gp.warping_function.f(Y)
+        y_norm = np.random.uniform(size=(5, 1), low=-1., high=1.)
+        y = wgp.norm.y.inverse_mean(y_norm)
+        Z_rogp = wgp.warp(y)
+        Z_gpy = wgp.gp.warping_function.f(y_norm)
 
         assert np.all(Z_rogp == Z_gpy)
 
-        GRAD_rogp = wgp.warp_deriv(Y)
-        GRAD_gpy = wgp.gp.warping_function.fgrad_y(Y)
+        GRAD_rogp = wgp.warp_deriv(y)
+        GRAD_gpy = wgp.gp.warping_function.fgrad_y(y_norm)
 
         assert np.all(GRAD_rogp == GRAD_gpy)
 