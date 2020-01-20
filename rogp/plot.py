c6aabc3578ce28fb6607f4ee3100202e24733e67
johwiebe <j.wiebe17@imperial.ac.uk>
2020-01-20 13:33:30 +0000

    Plot warping function.

--- rogp/plot.py
+++ rogp/plot.py
@@ -7,3 +7,8 @@ def plot(gp):
     GPy.plotting.change_plotting_library('plotly_offline')
     fig = gp.plot()[0]
     pltoff.plot(fig)
+
+def plot_warping(gp):
+    GPy.plotting.change_plotting_library('plotly_offline')
+    fig = gp.warping_function.plot()[0]
+    pltoff.plot(fig)
