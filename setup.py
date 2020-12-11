f586bec6c24799d5cb8089137a1b9acc85400569
johwiebe <j.wiebe17@imperial.ac.uk>
2020-12-11 09:01:51 +0000

    Add GPy to requirements.

--- setup.py
+++ setup.py
@@ -1,7 +1,11 @@
 #!/usr/bin/env
 from setuptools import setup, find_packages
+
 setup(
-    name="ROGP",
-    version="0.1dev",
-    packages=['rogp']
+    name='ROGP',
+    version='0.1dev',
+    author='Johannes Wiebe',
+    author_email='j.wiebe17@imperial.ac.uk',
+    packages=['rogp'],
+    install_requires=['GPy']
 )
